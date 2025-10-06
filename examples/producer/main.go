81a6cff5289acc34eb5789a281cdf8ef258a247a
Anders Kjeldsen <kjeldsenanders@gmail.com>
2025-10-06 08:08:29 +0200

    producer example

--- examples/producer/main.go
+++ examples/producer/main.go
@@ -15,21 +15,26 @@ import (
 // go run main.go -password=supersecret
 func main() {
 	// Get password from command line arguments
+	log.Println("Fetching password from command line arguments")
 	password := flag.String("password", "", "Kafka password")
 	flag.Parse()
 
+	log.Println("Setting up credentials")
 	// Setup credentials
 	credentials := dskafka.ClientCredentials{
 		Username: "ds.consumption.ingress.v1",
 		Password: *password,
 	}
 
+	log.Println("Getting bootstrap servers")
 	// Get bootstrap servers for your environment
 	bootstrapServers := dskafka.GetBootstrapServers(dskafka.Dev, false) // or dskafka.Dev
 
+	log.Println("Creating producer configuration")
 	// Create producer configuration
 	config := dskafka.DefaultProducerConfig(credentials, bootstrapServers)
 
+	log.Println("Creating producer")
 	// Create producer
 	producer, err := dskafka.NewProducer(config)
 	if err != nil {
@@ -41,6 +46,7 @@ func main() {
 		}
 	}()
 
+	log.Println("Creating event")
 	// Create an event
 	event := models.EventJson{
 		Id:          uuid.New(),
@@ -56,12 +62,14 @@ func main() {
 		Payload:     &map[string]interface{}{"userId": 123, "email": "user@example.com"},
 	}
 
+	log.Println("Sending event")
 	// Send single event
 	err = producer.SendEvent(context.Background(), "ds.workflow.pipeline.job.requested.v1", event)
 	if err != nil {
 		log.Printf("Failed to send event: %v", err)
 	}
 
+	log.Println("Sending event with custom headers")
 	// Send with custom headers
 	headers := []dskafka.Header{
 		{Key: "source", Value: "my-service"},
@@ -71,4 +79,5 @@ func main() {
 	if err != nil {
 		log.Printf("Failed to send event with headers: %v", err)
 	}
+	log.Println("Done")
 }
