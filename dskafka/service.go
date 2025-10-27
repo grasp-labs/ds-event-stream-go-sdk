6e3e87bf2df65e84c96f007be66398418cbcff2d
Anders Kjeldsen <kjeldsenanders@gmail.com>
2025-10-27 09:05:46 +0100

    formatting

---
+++ dskafka/service.go
@@ -0,0 +1,9 @@
+package dskafka
+
+// This file is now deprecated. All functionality has been moved to:
+// - common.go - Common types, configs, and utilities
+// - producer.go - Producer struct and methods
+// - consumer.go - Consumer struct and methods
+//
+// This file will be removed in a future version.
+// Please import from the specific files instead of service.go
